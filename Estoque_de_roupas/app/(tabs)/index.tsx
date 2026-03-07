61984d481a2d2d0c409bc834535a1a91028a6386
welli363 <toncosta08@gmail.com>
2026-03-07 11:53:08 -0300

    ;

--- Estoque_de_roupas/app/(tabs)/index.tsx
+++ Estoque_de_roupas/app/(tabs)/index.tsx
@@ -4,12 +4,34 @@ import {LinearGradient} from "expo-linear-gradient";
 
 export default function HomeScreen() {
   return (
-    <View>
-      <Text>Index.tsx</Text>
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Text>Olá, Lojista</Text>
+        <Text>Visão Geral</Text>
+        <View>
+          <Text>Saldo Atual</Text>
+          <Text>R$ 0,00</Text>
+          <View>
+            <Text>Entradas</Text>
+            <Text>R$ 0,00</Text>
+          </View>
+          <View>
+            <Text>Saídas</Text>
+            <Text>R$ 0,00</Text>
+          </View>
+        </View>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+    container:{
+      flex: 1,
 
+    },
+    header:{
+      borderWidth: 1,
+      borderColor: "#000"
+    }
 });
