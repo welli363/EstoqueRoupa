668df080bc292ac717726d574a626d3176dd2b67
welli363 <toncosta08@gmail.com>
2026-04-20 23:45:51 -0300

    comecei a fazer a págino do estoque.tsx
    Tive um pequeno progresso

--- Estoque_de_roupas/app/(tabs)/estoque.tsx
+++ Estoque_de_roupas/app/(tabs)/estoque.tsx
@@ -1,11 +1,72 @@
-import { StyleSheet, Text, View } from "react-native";
-import MaterialCommunityIcons from "@expo/vector-icons/MaterialCommunityIcons";
-import {LinearGradient} from "expo-linear-gradient";
+import { StyleSheet, Text, View, TextInput, Button, FlatList} from "react-native";
+import { useState } from "react";
 
-export default function HomeScreen() {
+
+
+type Produto = {
+  id: string;
+  nome: string;
+  tamanho: string;
+  quantidade: string;
+};
+
+
+export default function EStoque() {
+    
+  const [nome, setNome] = useState(''); 
+  const [tamanho, setTamanho] = useState('');
+  const [quantidade, setQuantidade] = useState('');
+  const [produtos, setProdutos] = useState<Produto[]>([]);
+
+  function adicionar() {
+    if(!nome || !tamanho || !quantidade) return;
+
+    const novoProduto = {
+      id: Date.now().toString(),
+      nome,
+      tamanho,
+      quantidade
+    };
+
+    setProdutos([...produtos, novoProduto]);
+    setNome('');
+    setTamanho('');
+    setQuantidade('');
+  } 
+
   return (
     <View>
-      <Text>estoque.tsx</Text>
+      <Text>ESTOQUE</Text>
+      <TextInput
+        placeholder="Nome do produto"
+        value={nome}
+        onChangeText={setNome}
+      />
+      <TextInput
+        placeholder="Tamanho (P, M, G..."
+        value={tamanho}
+        onChangeText={setTamanho}
+      />
+      <TextInput
+        placeholder="Quantidade"
+        value={quantidade}
+        onChangeText={setQuantidade}
+        keyboardType="numeric"
+      />
+
+      <Button title="Adicionar" onPress={adicionar}/>
+
+      <FlatList
+        data={produtos}
+        keyExtractor={(item) => item.id}
+        renderItem ={({item}) => (
+          <Text>
+            {item.nome} - {item.tamanho} - {item.quantidade}
+          </Text>
+        )}
+      />
+      
+
     </View>
   );
 }
--- Estoque_de_roupas/app/(tabs)/index.tsx
+++ Estoque_de_roupas/app/(tabs)/index.tsx
@@ -4,34 +4,11 @@ import {LinearGradient} from "expo-linear-gradient";
 
 export default function HomeScreen() {
   return (
-    <View style={styles.container}>
-      <View style={styles.header}>
-        <Text>Olá, Lojista</Text>
-        <Text>Visão Geral</Text>
-        <View>
-          <Text>Saldo Atual</Text>
-          <Text>R$ 0,00</Text>
-          <View>
-            <Text>Entradas</Text>
-            <Text>R$ 0,00</Text>
-          </View>
-          <View>
-            <Text>Saídas</Text>
-            <Text>R$ 0,00</Text>
-          </View>
-        </View>
-      </View>
-    </View>
+    <>
+    </>
   );
 }
 
 const styles = StyleSheet.create({
-    container:{
-      flex: 1,
 
-    },
-    header:{
-      borderWidth: 1,
-      borderColor: "#000"
-    }
 });
